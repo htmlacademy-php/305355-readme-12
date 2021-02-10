2695ef9f35c1ced1c148a18e6791da972ce8d7ab
oleg <kot-pirat@bk.ru>
2021-02-10 20:42:56 +0300

    Исправление замечаний

--- index.php
+++ index.php
@@ -41,6 +41,12 @@ $posts = [
   ],
 ];
 
+/**
+ * Возвращает урезанную строку, если строка длиннее некоторого числа
+ * @param string $text Полная строка
+ * @param int $max_symbols Число, ограничивающее количество символов
+ * @return string HTML с полной или урезанной строкой
+ */
 function limit_text ($text, $max_symbols=300) {
   $words = explode(' ', $text);
   $first_words = [];
@@ -48,7 +54,7 @@ function limit_text ($text, $max_symbols=300) {
   $is_more_than_max_symbols = false;
 
   foreach ($words as $word) {
-    $words_length += strlen(utf8_decode($word)) + 1;
+    $words_length += mb_strlen($word) + 1;
     $first_words[] = $word;
 
     if ($words_length - 1 >= $max_symbols) {
