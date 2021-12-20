e02f13fdd863ab18337494f89ef15a58fdf966f7
oleg <kot-pirat@bk.ru>
2021-12-20 18:04:08 +0300

    adding sql to php scenario

--- index.php
+++ index.php
@@ -7,54 +7,39 @@ $is_auth = rand(0, 1);
 
 $user_name = 'Oleg'; // укажите здесь ваше имя
 
-$posts = [
-  [
-    'title' => 'Цитата',	
-    'type' => 'post-quote',	
-    'content' => 'Мы в жизни любим только раз, а после ищем лишь похожих',
-    'name' => 'Лариса',
-    'avatar' => 'userpic-larisa-small.jpg',
-    'quote_author' => 'Неизвестный автор'
-  ],
-  [
-    'title' => 'Игра престолов',
-    'type' => 'post-text',
-    'content' => 'Не могу дождаться начала финального сезона своего любимого сериала!',
-    'name' => 'Владик',
-    'avatar' => 'userpic.jpg',
-  ],
-  [
-    'title' => 'Наконец, обработал фотки!',
-    'type' => 'post-photo',
-    'content' => 'rock-medium.jpg',
-    'name' => 'Виктор',
-    'avatar' => 'userpic-mark.jpg',
-  ],
-  [
-    'title' => 'Моя мечта',
-    'type' => 'post-photo',
-    'content' => 'coast-medium.jpg',
-    'name' => 'Лариса',
-    'avatar' => 'userpic-larisa-small.jpg',
-  ],
-  [
-    'title' => 'Лучшие курсы',
-    'type' => 'post-link',
-    'content' => 'www.htmlacademy.ru',
-    'name' => 'Владик',
-    'avatar' => 'userpic.jpg',
-  ],
-];
+$con = mysqli_connect('localhost', 'root', 'root', 'readme');
 
+if ($con == false) {
+  print('Ошибка подключения: ' . mysqli_connect_error());
+}
+else {
+  // print('Соединение установлено');
+  mysqli_set_charset($con, 'utf8');
+  $content_types_sql = 'SELECT * FROM content_types';
+  $posts_sql = 'SELECT p.title AS title, text_content, avatar, video_src, image_src, page_src, quote_author, u.name AS user_name, ct.class_name AS content_type
+    FROM posts p JOIN users u ON p.user_id = u.id JOIN content_types ct ON p.content_type_id = ct.id ORDER BY p.views_amount DESC';
+  
+  $content_types_result = mysqli_query($con, $content_types_sql);
+  $posts_result = mysqli_query($con, $posts_sql);
 
+  if (!$content_types_result || !$posts_result) {
+    $error = mysqli_error($con);
+    print('Ошибка запроса: ' . $error);
+  }
+  else {
+    // print('Успешный запрос');
+    $content_types = mysqli_fetch_all($content_types_result, MYSQLI_ASSOC);
+    $posts = mysqli_fetch_all($posts_result, MYSQLI_ASSOC);
 
-$main_content = include_template('main.php', ['posts' => $posts]);
-$layout = include_template('layout.php', [
-    'is_auth' => $is_auth,
-    'user_name' => $user_name,
-    'main_content' => $main_content,
-    'page_title' => 'readme: популярное',
-  ]
-);
-echo $layout;
+    $main_content = include_template('main.php', ['posts' => $posts, 'content_types' => $content_types]);
+    $layout = include_template('layout.php', [
+        'is_auth' => $is_auth,
+        'user_name' => $user_name,
+        'main_content' => $main_content,
+        'page_title' => 'readme: популярное',
+      ]
+    );
+    echo $layout;
+  }
+}
 ?>
